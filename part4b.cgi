df90d95c4350b1b0dcf4c08c7353d72ddeef3fe5
Lu Wang <lu.irena.wang@gmail.com>
2016-10-01 19:30:00 -0400

    working part4, properly named

--- part4b.cgi
+++ part4b.cgi
@@ -4,5 +4,5 @@
 #
 echo "Content-type: text/html"
 echo ""
-( cat part4b.top ; cat ˜lib113/hw/tt2ht/who.output ) | ./tt2ht2
+( cat part4b.top ; cat ~lib113/hw/tt2ht/who.output ) | ./tt2ht2
 echo "</table></body></html>"
--- tt2ht2.c
+++ tt2ht2.c
@@ -1,101 +1,153 @@
 #include	<stdio.h>
-
-/*
-
-
-
-
-
-*/
-
+#include 	<string.h>
 
 /*
 Methodology: 
 array of strings for attributes 
-read linput line by line using fgets() 
+read lattr line by line using fgets() 
 if noprocess, just print 
 if attribute, store entire attribute into a row in my array 
 if process, then do exactly what the previous code did 
 */ 
 
 
-/*	
-	1. Beginning of file: <table><tr>. If not EOF -> 2. If EOF -> 5 
-	2. Create cells: <td> text until space, then </td>. -> 6 When encounter \n -> 4. When EOF -> 5
-	3. In whitespace: Nothing until text -> 3 
-	4. End of row: </tr> -> 2. 
-	5. End of file: </tr> </table> 
+/*
+Program states: 
+1. process block (default). Create html table. If attribute -> 2, if noprocess -> 3
+2. in attribute block. Store attributes to attr. if /attribute -> 1
+3. in noprocess block. print lines. if /noprocess -> 1
+
+For part4, I'm going to assume properly formatted input. 
+If I encounter an <attributes> inside a noprocess block, I will print it. .
+If I encounter a <noprocess> inside an attribute block, I will also print it. 
+In other words, the attribute state can only be entered from the default state.
+The noprocess state is greedy - this can be invoked from any state. 
 */
 
-int process(int cur, int state); 
+#define DELIM ' '
+int MAXLINES=300; 
+int MAXLEN=300; 
+void process(char [], char attr[MAXLINES][MAXLEN] ); 
+int get_state(int state, char []); 
+
+int split_line( char orig[], char fields[MAXLINES][MAXLEN] );
+
 
 void main()
 {
 	int cur; 
-    int state=1;
-	while ( (cur = getchar()) != EOF) 
-    {
-		state = process(cur, state) ; 
-	} 
-	
+    int oldstate=1; // the state of the cursor when processing text
+	int pstate=1; //the state of the program
+	int tag=0; 
+
+	char attr[MAXLINES][MAXLEN]; 
+
+	char line[MAXLEN]; 
+	int attr_index = 0; 
+
+	while( fgets(line, MAXLEN, stdin) ) { 
+		/* keep \n and add \0 to end of line */
+
+		oldstate = pstate; 
+		pstate = get_state(pstate, line); 
+
+		if (oldstate != pstate) tag = 1; 
+		
+		if (tag ==1) { 
+		//do nothing when the line IS the tag 
+		tag = 0;  
+		} 
+		
+        else if (pstate ==2) // store attribute 
+        { 
+            strcpy(attr[attr_index], line); 
+            attr[attr_index][strlen(line)-1] = '\0'; 
+            attr_index++; 
+        } 
+		else if (pstate ==3) // no process
+		{
+			printf("%s", line); 
+		} 
+		else //default format table 
+		{
+			process(line, attr); 
+		}
+		
+	}
 } 
 
-
-
-int process(int cur, int state)  
+int get_state(int curstate, char line[]) 
 {
-    if (state == 1) // beginning of table 
-    {
-		printf("<table>"); 
-		if (cur != ' ') { 
-			state = 2; 
-			printf("\n\t"); 
-			printf("<tr>\n\t\t<td>"); 
-			putchar(cur); 
-		} 
-		
-    } 
-    else if (state == 2) // generating a row 
-    { 
-        
-		if (cur == '\n') { // reached end of row 
-			printf("</td>\n\t</tr>"); 
-			state = 4; 
-		} 
-		else if (cur != ' ') {
-			putchar(cur); 
-		} 
-		else if (cur == ' ') { // reached whitespace 
-			printf("</td>"); 
-			state = 3; 
-		} 
-		
-    } 
-    else if (state == 3)  // what to do while whitespace 
-    {
-		if (cur != ' ') { 
-			printf("<td>"); 
-			putchar(cur); 
-			state = 2; 
-		} 
-    }
-	else if (state == 4) // begin new row 
-    {
-		
-		if (cur != ' ') { 
-			state = 2; 
-			printf("\n\t"); 
-			printf("<tr>\n\t\t<td>"); 
-			putchar(cur); 
-		} 
-    }
-    else {
-		state =5 ;
-		printf("\n");
-		printf("</table>")  ;
+	
+	if ((curstate == 1) && (strstr(line, "<attributes>") ))  { 
+
+		return 2; 
+	} 
+	if ((curstate == 2) && (strstr(line, "</attributes>") )) { 
+
+		return 1; 
+	} 
+	if (strstr(line, "<noprocess>") ) { 
+
+		return 3; 
+	} 
+	if (strstr(line, "</noprocess>") ) { 
+
+		return 1; 
+	} 
+	return curstate; 
+}
+
+
+void process(char line[], char attr[MAXLINES][MAXLEN]) 
+{ 
+	char row[MAXLINES][MAXLEN];
+	int columns; 
+	columns = split_line(line, row); 
+	printf("\n\t<tr>"); 
+	for (int i = 0; i < columns; i++)
+	{ 
+		printf("\n\t\t<td %s> %s </td>", attr[i], row[i]); 
 	} 
 
-	return state ;
+	printf("\n\t</tr>\n"); 
+} 
+
+int
+split_line( char line[], char row[MAXLINES][MAXLEN] )
+/*
+
+*/
+{
+	int in_text=0; 
+	int column_index = 0; 
+	int cell_index =0; 
+
+	for (int line_index=0; line_index < strlen(line)+1; line_index++)
+	{
+		if ((line[line_index] == ' ') && (in_text ==1)) //hit a space 
+		{ 			
+			in_text = 0;
+			row[column_index][cell_index] = '\0'; 
+			cell_index = 0; 
+			column_index++; 			
+		} 
+		else if (line[line_index] == '\0') //reached end of the line 
+		{ 
+			column_index++; //finished last column 
+			row[column_index][cell_index] = line[line_index]; //copy trailing \0	
+
+			return column_index; 
+		} 
+		else if ((line[line_index] != ' ') && (line[line_index] != '\n') ) //cell contents 
+		{
+			in_text = 1; 
+			row[column_index][cell_index] = line[line_index]; 
+			cell_index++; 
+		}
+	}
+
 
 }
+
 
